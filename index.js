a94d2c3b9290f9e70d7d6c2746b8668497d45e79
thariq isyraq <thariqisyraq09@gmail.com>
2023-09-27 23:58:38 +0800

    saya menghapus fitur todolist dan memindahkannya ke branch todo

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 // feather icons
 feather.replace();
 // feather icons
-
-const listItems = document.querySelectorAll(".list-items");
-
-for(i = 0; i < listItems.length; i++) {
-    listItems[i].style.color= "brown";
-} 
-console.log(listItems);
-
-const ul = document.querySelector("ul");
-const li = document.createElement("li");
-
-ul.append(li); 
-
