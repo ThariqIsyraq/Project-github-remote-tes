b5cf58d034c13400e840fd36e1631af4991e8282
thariq isyraq <thariqisyraq09@gmail.com>
2023-10-24 22:41:22 +0800

    im just adding a few more styles

--- index.js
+++ index.js
@@ -2,3 +2,30 @@
 feather.replace();
 // feather icons
 
+const tambahin = document.getElementsByClassName("tambahin")[0];
+
+tambahin.addEventListener('click', function () {
+    const diva = document.getElementById("a");
+    const pBaru = document.createElement("p");
+    pBaru.textContent = "halohehehe" 
+
+    diva.appendChild(pBaru);
+})
+
+// const tambahin = document.querySelector(".tambahin");
+
+// if (tambahin) {
+//   tambahin.addEventListener('click', addNewParagraph);
+// }
+
+// function addNewParagraph() {
+//   const diva = document.getElementById("a");
+
+//   if (diva) {
+//     const pBaru = document.createElement("p");
+//     const text = document.createTextNode("halohehe");
+//     pBaru.appendChild(text);
+//     diva.appendChild(pBaru);
+//   }
+// }
+
